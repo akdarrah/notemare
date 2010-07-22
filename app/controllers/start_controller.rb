ee35cea3d31fa549eb6149fa297ad12aef3550d0
Adam Darrah <akdarrah@umail.iu.edu>
2010-07-21 21:57:00 -0400

    refactored song action

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -4,13 +4,16 @@ class StartController < ApplicationController
   # FEATURE:: add similar artists feature
   # FEATURE:: make sure songs belong to artist
   # FEATURE:: throw an error when artist not found
-  # FEATURE:: make update delayed jobs
   # FEATURE:: add 'Mix' model for saving playlists
-  # FEATURE:: style app to look cool
   # FEATURE:: add embed code into site and share links
   # FEATURE:: add amazon adds/link to lastfm profile
   
-  # CHORE:: link styles
+  # FEATURE:: site styles
+  # FEATURE:: inline application helpers
+  # FEATURE:: add admin section
+    # Audit artist form
+    # Delayed Jobs interface
+  # CHORE:: make artist model store source_url so it can be overriden
   
   layout 'base.html.haml'
   
@@ -22,53 +25,29 @@ class StartController < ApplicationController
     artists = params[:artist][:name].split(',')
     @code = ""
     
-    # if only one artist was entered, use the default algorithm
-    if artists.length == 1
-      # determine whether to randomize the results or not
-      # if user inserted a ! into input, remove it
-      rand = true if artists[0][-1,1] == '!'
-      artists[0] = artists[0].split("!")[0] if rand
-      
-      # determine where to fetch the artist song data from (either lastfm or localhost)
-      # and return a string of grooveshark ids to be injected into the player
-      # first make a query to tinysong using the given artist name
-      lookup_data = JSON.parse(open("http://tinysong.com/b/" + to_URL(artists[0]) + "?format=json").read)
-
+    # determine whether to randomize the results or not
+    # if user inserted a ! into input, remove it
+    rand = true if artists[0][-1,1] == '!'
+    artists[0] = artists[0].split("!")[0] if rand
+    
+    # loop through each artist given and compile a string of all shark codes
+    artists.each do |instance|
+      # make a query to tinysong using the given artist name
+      lookup_data = JSON.parse(open("http://tinysong.com/b/" + to_URL(instance) + "?format=json").read)
       # if lookup_data is an empty set you do not have a valid artist
       unless lookup_data == []
         @artist = Artist.find_or_create_by_name(to_URL(lookup_data['ArtistName']))
-        if @artist.expired? || @artist.data.nil? || @artist.similar_data.nil? || @artist.shark_code.nil?
+        @artist.expired?
+        if @artist.data.nil? || @artist.similar_data.nil? || @artist.shark_code.nil?
           @artist.fetch
         end
-        @code = @artist.get_data[:code]
-        @code = randomize(@code) if rand == true
-        respond_to do |format|
-          format.js { render :partial => 'songs.js.erb' }
-        end
+        @code << @artist.get_data[:code]
       end
-    else
-      # More than 1 artist was entered
-      # loop through each artist given and compile a string of all shark codes
-      artists.each do |instance|
-        # determine where to fetch the artist song data from (either lastfm or localhost)
-        # and return a string of grooveshark ids to be injected into the player
-        # first make a query to tinysong using the given artist name
-        lookup_data = JSON.parse(open("http://tinysong.com/b/" + to_URL(instance) + "?format=json").read)
-        # if lookup_data is an empty set you do not have a valid artist
-        unless lookup_data == []
-          # UPDATE REQUIRED
-          @artist = Artist.find_or_create_by_name(to_URL(lookup_data['ArtistName']))
-          if @artist.expired? || @artist.data.nil? || @artist.similar_data.nil? || @artist.shark_code.nil?
-            @artist.fetch
-          end
-          @code << @artist.get_data[:code]
-        end
-      end
-      # respond to request after all artists have been iterated
-      @code = randomize(@code)
-      respond_to do |format|
-        format.js { render :partial => 'songs.js.erb' }
-      end
+    end
+    # respond to request after all artists have been iterated
+    @code = randomize(@code) if artists.length > 1 || rand
+    respond_to do |format|
+      format.js { render :partial => 'songs.js.erb' }
     end
   end
   
