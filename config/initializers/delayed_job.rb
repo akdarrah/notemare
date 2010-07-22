871d9e26f20ba9fa3ee4931caa85b146e0032708
Adam Darrah <akdarrah@umail.iu.edu>
2010-07-21 22:18:12 -0400

    made delayed job worker start automatically on deploy

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,8 +1,12 @@
 require 'delayed_job'
-# config/initializers/delayed_job.rb
+
 Delayed::Worker.backend = :active_record
+
 # Delayed::Job.destroy_failed_jobs = false
 # silence_warnings do
 #   Delayed::Job.const_set("MAX_ATTEMPTS", 5)
 #   Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
 # end
+
+# Start delayed jobs
+system "cd #{Rails.root} && script/worker start"
