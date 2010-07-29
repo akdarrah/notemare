506b28553f815e47f8e6fe2ba9d503ff989a485d
Adam Darrah <akdarrah@umail.iu.edu>
2010-07-29 01:21:59 -0400

    decided against authlogic for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.login "login", :controller => "user_sessions", :action => "new"
-  map.logout "logout", :controller => "user_sessions", :action => "destroy"
-
-  map.resources :user_sessions
-  map.resources :users
   map.root :controller => "artist"
   
   map.connect ':controller/:action/:id'
